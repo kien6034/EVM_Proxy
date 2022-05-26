ebcbbb1cd29618ab9137a96ecab341903bfa593c
kien6034 <trinhtrungkientb99@gmail.com>
2022-05-26 11:47:18 +0700

    remove node modules

--- node_modules/@openzeppelin/hardhat-upgrades/dist/force-import.d.ts
+++
@@ -1,8 +0,0 @@
-import type { HardhatRuntimeEnvironment } from 'hardhat/types';
-import type { ContractFactory, Contract } from 'ethers';
-import { Options } from './utils';
-export interface ForceImportFunction {
-    (proxyAddress: string, ImplFactory: ContractFactory, opts?: Options): Promise<Contract>;
-}
-export declare function makeForceImport(hre: HardhatRuntimeEnvironment): ForceImportFunction;
-//# sourceMappingURL=force-import.d.ts.map
